defed85f19be03106f628cf4967cf61e588f8693
Main <example@mail.com>
2024-04-02 12:53:40 +0200

    After adding fixtures for goods

--- goods/admin.py
+++ goods/admin.py
@@ -1,3 +1,14 @@
+from atexit import register
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+@admin.register(Categories)
+class CategoriesAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
+
+@admin.register(Products)
+class ProductsAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name', )}
