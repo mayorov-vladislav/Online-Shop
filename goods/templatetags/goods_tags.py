d7387870a2779bcb0fe2df13d57da64346be3089
Main <example@mail.com>
2024-04-04 17:46:52 +0200

    The product filter on the site page  and urlencode

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.http import urlencode
 from goods.models import *
 
 
@@ -8,3 +9,10 @@ register = template.Library()
 @register.simple_tag()
 def tag_categories():
     return Categories.objects.all()
+
+
+@register.simple_tag(takes_context=True)
+def chang_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
