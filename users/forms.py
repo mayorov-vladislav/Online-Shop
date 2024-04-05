5502569155208fe69cd4e741f129351c27fce473
Main <example@mail.com>
2024-04-05 20:53:31 +0200

    Profile form, controller & notifications are ready

--- users/forms.py
+++ users/forms.py
@@ -1,21 +1,20 @@
 from dataclasses import fields
 from logging import PlaceHolder
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 from users.models import *
 
 
 class UserLoginForm(AuthenticationForm):
-
-    username = forms.CharField()
-    password = forms.CharField()
-
     class Meta:
             model = User
             fields = [
                 'username',
                 'password',
             ]
+
+    username = forms.CharField()
+    password = forms.CharField()
 
 
 class UserRegistrationForm(UserCreationForm):
@@ -36,3 +35,21 @@ class UserRegistrationForm(UserCreationForm):
     email = forms.CharField()
     password1 = forms.CharField()
     password2 = forms.CharField()
+
+
+class ProfileForm(UserChangeForm):
+    class Meta:
+        model = User
+        fields = (
+            "image",
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+        )
+
+    image = forms.ImageField(required=False)
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
