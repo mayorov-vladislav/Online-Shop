ff167341d39aa0e2c70ba239f78652aa41d90066
Main <example@mail.com>
2024-04-06 11:39:25 +0200

    Create order template and form prepared

--- users/models.py
+++ users/models.py
@@ -6,6 +6,7 @@ from django.forms import ImageField
 # Create your models here.
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', blank=True, null=True, verbose_name='Изображение')
+    phone_number = models.CharField(max_length=10, blank=True, null=True)
 
     class Meta:
         db_table = 'user'
