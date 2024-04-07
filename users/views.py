af4689ffe0a555d247236b3e212c9f579fbcff0d
Main <example@mail.com>
2024-04-07 19:10:55 +0200

    Final edits

--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 from email import message
 from django.contrib.auth.decorators import login_required
 from django.contrib import auth, messages
+from django.db.models import Prefetch
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from carts.models import Cart
+import orders
+from orders.models import Order, OrderItem
 from users.forms import *
 
 
@@ -78,9 +81,21 @@ def profile(request):
     else:
         form = ProfileForm(instance=request.user)
 
+    orders = (
+        Order.objects.filter(user=request.user)
+        .prefetch_related(
+            Prefetch(
+                'orderitem_set',
+                queryset=OrderItem.objects.select_related('product'),
+            )
+        )
+        .order_by('-id')
+    )
+
     context = {
         'title': 'Home - Кабинет',
         'form': form,
+        'orders': orders
     }
 
     return render(request, 'users/profile.html', context)
