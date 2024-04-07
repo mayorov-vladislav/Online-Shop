6840f9f4c6faa2396cfbf1953b7227a6d8f3bd6f
Main <example@mail.com>
2024-04-08 01:56:27 +0200

    Full site

--- goods/utils.py
+++ goods/utils.py
@@ -7,7 +7,8 @@ def q_search(query):
 
     if query.isdigit() and len(query) <= 5:
         return Products.objects.filter(id=int(query))
-
+        
+        
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
 
